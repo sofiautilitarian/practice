aebc6aa479c76cb9539e69f77f812fd174d8e6f6
Sofia Noor Rafa <91686970+sofiautilitarian@users.noreply.github.com>
2025-03-08 01:19:12 +0600

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Message from "./Message";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    Message('Adam')
+  );
+}
+
+export default App;
---
+++ Message.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+function Message(Name: string) {
+    return <div>Hello this is {Name}</div>;
+}
+export default Message;
