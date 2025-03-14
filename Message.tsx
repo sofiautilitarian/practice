db241388bb1e7014326a5276d68cbfa9f79ac616
Sofia Noor Rafa <91686970+sofiautilitarian@users.noreply.github.com>
2025-03-14 14:39:38 +0600

    Add files via upload

--- Message.tsx
+++ Message.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-
-function Message(Name: string) {
-    return <div>Hello this is {Name}</div>;
+//PascalCasing
+function Message() {
+    //JSX: JavaScript XML
+    var whatever = 'Sofia';
+    return <h1>Hello World. This is {whatever}</h1>;
 }
 export default Message;
