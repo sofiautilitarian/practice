5cd6dd818cab0f1f80554c8fc4213c98a47b92bf
Sofia Noor Rafa <91686970+sofiautilitarian@users.noreply.github.com>
2025-03-16 06:29:45 +0600

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,41 +1,16 @@
-import { useState } from "react";
-import { BsBatteryFull } from "react-icons/bs";
-
-//import Message from "./Message";
-//import reactLogo from "./assets/react.svg";
-//import bootstrap from 'node_modules\bootstrap\dist\css\bootstrap.css';
-//If we click on this button, we should treat state objects as immutable or readable
-import ListGroup from "./ListGroup";
-import Button from "./Button";
-
+import ListGroup from './components/ListGroup';
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
-  });
-  const handleClick = () => {
-    // const newDrink = {
-    //   title: drink.title,
-    //   price: 6
-    // }
-    setDrink({...drink, price: 6});
-    //setDrink(newDrink);
-  };
-  // const [firstName, setFirstName] = useState("");
-  // const [lastName, setLastName] = useState("");
-  // const [isLoading, setLoading] = useState(false);
-  // const [person, setPerson] = useState({
-  //   firstName: "",
-  //   secondName: "",
-  // });
-  // const fullName = firstName + " " + lastName;
-  // return <div>{fullName}</div>;
-  return (
-    <div>
-      {drink.price}
-      <button onClick={handleClick}>Click Me</button>
-    </div>
-  );
+  //list of items moved from ListGroup to App
+  let items = [
+      "America",
+      "Zaman Razeen",
+      "Sami had an ex",
+      "Syeda Sowad",
+      "Hasnat Jami",
+      "Nasheen Abdullah",
+    ];
+  //two compilation errors
+  //typescript always alerts about errors during compile time
+  return <div><ListGroup items = {items} heading = {'Cities'}/></div>;
 }
-
 export default App;
